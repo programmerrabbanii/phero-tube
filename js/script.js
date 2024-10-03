b56a7acfe6ff35b8bfc0710b4f75c6ee58239156
programmerrabbanii <rabbanisarkar642@gmail.com>
2024-10-03 12:15:37 +0600

    added no data found section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+function setTime(time){
+    const hour=parseInt(time/3600)
+    let remaingSecond=time%3600;
+    const minutes=parseInt(remaingSecond /60)
+    remaingSecond=remaingSecond %60;
+
+    return`${hour} hour ${minutes} minute ${remaingSecond} second ago`
+
+
+}
+
+
+
 const loadCategories=()=>{
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then(response=>response.json())
@@ -12,27 +25,63 @@ const loadVideos=()=>{
     .catch(error=>console.log(error))
 }
 
+const loadSortVide=(id)=>{
+
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(response=>response.json())
+    .then(data=>loadVidesDisply(data.category))
+    .catch(error=>console.log(error))
+
+
+}
+
 const loadVidesDisply=(videos)=>{
     const videoShowElement=document.getElementById('displyVideo')
+    videoShowElement.innerHTML=""
+    if(videos.length==0){
+        videoShowElement.classList.remove('grid');
+        videoShowElement.innerHTML=`
+        <div class="min-h-[300px] flex  flex-col gap-5 justify-center items-center">
+        <img src="./assect/Icon.png"/>
+        <h2 class="text-center text-3xl"> Oops!! Sorry, There is no </br> content here </h2>
+
+        </div>
+        
+        `
+        return
+    }
+    else{
+        videoShowElement.classList.add('grid')
+    }
   videos.forEach((item)=>{
     console.log(item);
     const card=document.createElement('div')
     card.classList=('card card-compact')
     card.innerHTML=`
-    <figure class="h-[200px]">
+    <figure class="h-[200px] relative">
     <img
       src=${item.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
+      ${item.others.posted_date?.length== 0 ? "" : `<span class="absolute right-2 bottom-2 bg-black text-white px-2 py-1"> ${setTime(item.others.posted_date)} </span>`}
+      
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2 flex items-center gap-2">
+    
+    <div>
+    <img class="w-10 h-10 rounded-full object-cover" src="${item.authors[0].profile_picture}"/>
+    </div>
+    <div>
+    <h2 class="font-bold">
+       ${item.title}
+    </h2>
+    <div class="flex items-center gap-2">
+    <p>${item.authors[0].profile_name}</p>
+    ${item.authors[0].verified=== true? `<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=63760&format=png"/>` : ''
+    }
+    </div>
     </div>
   </div>
-    
     `
     videoShowElement.append(card)
    
@@ -42,10 +91,15 @@ const loadVidesDisply=(videos)=>{
 const loadDisplay=(categories)=>{
     const disPlayNavBar=document.getElementById('showNavbar')
     categories.forEach((items)=>{
-        const button=document.createElement('button')
-        button.classList.add('btn')
-        button.innerText=items.category
-        disPlayNavBar.appendChild(button)
+        const buttonContainer=document.createElement('div')
+         buttonContainer.innerHTML=`
+         <button onclick="loadSortVide(${items.category_id})" class="btn">
+         ${items.category}
+         
+         </button>
+         `
+       
+        disPlayNavBar.appendChild(buttonContainer)
         
     })
 }
